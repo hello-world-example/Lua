4389fc8aaf91de3b8551723600a7e7b76ca82819
kaibin.yang <ykb553@163.com>
2020-11-27 15:37:17 +0800

    fix

--- src/hello.lua
+++ src/hello.lua
@@ -1,14 +1,11 @@
--- 定义一个名为 module 的模块
 module = {}
- 
--- 
-local function module.sayHello()
+
+function module.sayHello()
     print("Hello World")
 end
- 
---
+
 function module.say(some)
     print(some)
 end
- 
+
 return module
